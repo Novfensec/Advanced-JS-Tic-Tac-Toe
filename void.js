44ffc1535864c03727523dc4a2f95697f4a7006a
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2022-01-29 15:17:14 +0530

    Update void.js

--- void.js
+++ void.js
@@ -1,3 +1,4 @@
+//declaring constants and variables
 const x_hover = "x-hover";
 const o_hover = "o-hover";
 const cell = document.querySelectorAll("[data-cell]");
@@ -5,13 +6,17 @@ const n_cell = document.querySelector("[inf-cell]");
 const x_npx = document.querySelector(".xnpx");
 const o_npx = document.querySelector(".onpx");
 const w_msg = document.querySelector(".w-msg");
+w_msg.addEventListener("click",rst);
 const d_wmsg = document.querySelector("[d-wmsg]");
 const s_trx = document.querySelector(".strx");
+s_trx.addEventListener("click",swap);
 let x_count = 0;
 let o_count = 0;
 let currentUp;
 let strx;
 let claSs;
+
+//declaring winning combinations
 const w_cbats = [
     [0, 1, 2],
     [3, 4, 5],
@@ -22,15 +27,17 @@ const w_cbats = [
     [0, 4, 8],
     [2, 4, 6],
 ];
+
+//starting game
 start()
+
+//declaring start function to start or update the game
 function start() {
-    strx = true;
     s_trx.style.display = "inline";
     claSs = o_hover;
     n_cell.textContent = x_hover.toUpperCase();
     x_npx.textContent = `X_npx: ${x_count}`;
     o_npx.textContent = `O_npx: ${o_count}`;
-
     cell.forEach((cell) => {
         cell.classList.remove(x_hover);
         cell.classList.remove(o_hover);
@@ -39,6 +46,8 @@ function start() {
         cell.addEventListener("click", handlrclick, { once: true });
     });
 }
+
+//declaring click event function
 function handlrclick(e) {
     strx = false;
     const cell = e.target;
@@ -60,9 +69,13 @@ function handlrclick(e) {
         start();
     }
 }
+
+//manipulate data in cell
 function call(cell, currentUp) {
     cell.textContent = currentUp;
 }
+
+//declaring function to return true if any winning combinations are formed by object node list or false if not
 function checkwin(claSs) {
     return w_cbats.some((combination) => {
         return combination.every((index) => {
@@ -70,15 +83,21 @@ function checkwin(claSs) {
         });
     });
 }
+
+//declaring function to return true if all cell have classes x-hover or o-hover and no w_cbats are formed
 function n_draw() {
     return [...cell].every((cell) => {
         return cell.classList.contains(x_hover) || cell.classList.contains(o_hover);
     });
 }
+
+//declaring function to swap turns before any data is manipulated in the cell
 function swap() {
     claSs = claSs == x_hover ? o_hover : x_hover;
     n_cell.textContent = claSs == o_hover ? x_hover.toUpperCase() : o_hover.toUpperCase();
 }
+
+//declaring function to make message element visible
 function rst() {
     w_msg.classList.remove("show");
 }
